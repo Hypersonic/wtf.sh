812d6a4107d7f92675c0c9cc9b0132b2dfd55395
Josh Hofing <joshhofing@gmail.com>
2016-07-01 22:21:05 -0400

    Fix script injection during cache creation... oops :)

--- post_functions.sh
+++ post_functions.sh
@@ -15,13 +15,13 @@ function create_post {
     local post_id=$(basename $(mktemp --directory posts/XXXXX));
 
 
-    echo ${username} > posts/${post_id}/1;
-    echo ${title} >> posts/${post_id}/1;
-    echo ${text} >> posts/${post_id}/1;
+    echo ${username} > "posts/${post_id}/1";
+    echo ${title} >> "posts/${post_id}/1";
+    echo ${text} >> "posts/${post_id}/1";
 
 
     # add to our cache for the homepage
-    echo "<li><a href=\"/post.wtf?post=${post_id}\">${title}</a> by ${username}</li>" >> .index_cache.html
+    echo "<li><a href=\"/post.wtf?post=${post_id}\">$(htmlentities <<< ${title})</a> by $(htmlentities <<< ${username})</li>" >> .index_cache.html
     echo ${post_id};
 
 }
